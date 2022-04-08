6892fc63d3696db0a1ede5d0985fa837d5232062
Samiya <skmcintosh20@gmail.com>
2022-04-08 16:06:55 -0700

    added power button

---
+++ src/subComponents/LogoComponent.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const Logo = styled.Logo``;
+
+const LogoComponent = () => {
+	return <Logo>LogoComponent</Logo>;
+};
+
+export default LogoComponent;
