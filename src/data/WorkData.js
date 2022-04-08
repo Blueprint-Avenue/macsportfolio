73044cdcd2b21f223f6146b8e8f4d79573ddd4bc
Samiya <skmcintosh20@gmail.com>
2022-04-08 13:05:30 -0700

    Initialized Project

---
+++ src/data/WorkData.js
@@ -0,0 +1,78 @@
+export const Work = [
+	{
+		id: 1,
+		name: "Pinterest Clone",
+		description: "A clone of Pinterest using React Native. ",
+		tags: ["reactnative", "typescript", "hooks"],
+
+		demo: "",
+		github: "https://github.com/IMackie22/pinterestclone",
+	},
+	{
+		id: 2,
+		name: "Wiz Shoes",
+		description: "A simple ecomerce shoe site using ReactNative and Redux.",
+		tags: ["reactnative", "redux"],
+
+		demo: "",
+		github: "https://github.com/IMackie22/wizshoes",
+	},
+	{
+		id: 3,
+		name: "Facebook Clone",
+		description:
+			"A clone of Facebook build using ReactJS. With realtime login from the fbdevelopers API.",
+		tags: ["react", "api", "tailwind"],
+
+		demo: "",
+		github: "https://github.com/IMackie22/fbclone",
+	},
+	{
+		id: 4,
+		name: "Real Homes App",
+		description:
+			"A simple modern real estate app using ReactJS and NextJs. With rental properties and for-sale properties. To using an API to fetch data.",
+		tags: ["react", "nextjs", "api"],
+
+		demo: "",
+		github: "https://github.com/IMackie22/realhomesapp",
+	},
+	{
+		id: 5,
+		name: "Todo App(ReactNative + Hooks)",
+		description: "A simple todo app build using Hooks.",
+		tags: ["reactnative", "hooks"],
+
+		demo: "",
+		github: "https://github.com/IMackie22/todoApp",
+	},
+	{
+		id: 6,
+		name: "Millionaire Quiz (React + Hooks)",
+		description: "A reponsive React millionaire quiz. Built from scratch.",
+		tags: ["react", "html", "css"],
+
+		demo: "",
+		github: "https://github.com/IMackie22/millionairequiz",
+	},
+	{
+		id: 7,
+		name: "Muzik Player",
+		description:
+			"A reponsive ReactJS music app that uses the spotify API. Built from scratch.",
+		tags: ["react", "api", "css"],
+
+		demo: "",
+		github: "https://github.com/IMackie22/muzikplayer",
+	},
+	{
+		id: 8,
+		name: "Uber Clone (ReactNative + Redux)",
+		description:
+			"A clone of Uber using ReactNative. With realtime fetching prices, location, and distance using the GoogleMaps API.",
+		tags: ["reactnative", "redux", "googlemapsapi"],
+
+		demo: "",
+		github: "https://github.com/IMackie22/uberclone",
+	},
+];
