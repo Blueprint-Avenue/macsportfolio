19d3e53fb14cd38067c32069c23f0d9fed8b2200
Samiya <skmcintosh20@gmail.com>
2022-04-08 14:55:34 -0700

    added components/pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { ThemeProvider } from "styled-components";
+import { lightTheme, DarkTheme } from "./components/Themes";
+import { Switch, Route } from "react-router";
+import "./globalStyles.css";
+import { Main } from "./components/Main";
+import AboutPage from "./components/AboutPage";
+import BlogPage from "./components/BlogPage";
+import WorkPage from "./components/WorkPage";
+import SkillsPage from "./components/SkillsPage";
+
 function App() {
-  return <div>
-    App File
-    </div>
-    
+	return (
+		<>
+			<ThemeProvider theme={lightTheme}>
+				<Switch>
+					<Route exact path="/" component={Main} />
+					<Route exact path="/about" component={AboutPage} />
+					<Route exact path="/blog" component={BlogPage} />
+					<Route exact path="/work" component={WorkPage} />
+					<Route exact path="/skills" component={SkillsPage} />
+				</Switch>
+			</ThemeProvider>
+		</>
+	);
 }
 
-export default App
-
+export default App;
---
+++ src/components/Themes.js
@@ -0,0 +1,18 @@
+export const lightTheme = {
+	body: "#EEEEEE",
+	text: "#F3E9DD",
+	fontFamily: "'Source Sans Pro', sans-serif",
+	bodyRgba: "	238, 238, 238",
+	textRgba: "	243, 233, 221",
+};
+
+export const DarkTheme = {
+	body: "#FF5F00",
+	text: "#F3E9DD",
+	fontFamily: "'Source Sans Pro', sans-serif",
+	textRgba: " 243, 233, 221",
+	bodyRgba: "	255, 95, 0",
+	backgroundColor: "#ff5f00",
+	backgroundImage:
+		"https://www.transparenttextures.com/patterns/basketball.png",
+};
